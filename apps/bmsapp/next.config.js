9da5293e5741715765e675c5a987d3e03e5d6d27
Agu Chux <agu.chux@yahoo.com>
2023-07-06 14:14:04 +0100

    EBMS App

--- apps/bmsapp/next.config.js
+++ apps/bmsapp/next.config.js
@@ -2,6 +2,8 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const path = require('path');
+
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
@@ -14,6 +16,12 @@ const nextConfig = {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
+  },
+  sassOptions: {
+    includePaths: [
+      path.join(__dirname, 'styles'),
+      path.join(__dirname, 'public', 'assets', 'scss'),
+    ],
   },
 };
 
