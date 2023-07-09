41c6515a15bea0dc7ca272ee344343b14b6df112
Agu Chux <agu.chux@yahoo.com>
2023-07-09 05:01:40 +0100

    BMS: Updated

--- apps/bmsapp/pages/dashboard/[module_name]/requested-accounts.tsx
+++ apps/bmsapp/pages/dashboard/[module_name]/requested-accounts.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
 import Layout from 'components/layout';
 import { NextPage } from 'next';
 import useSWR from 'swr';
 import { fetcher } from 'libs';
-import { RequestedAccountsTable } from 'components/datatables/user';
+import { RequestedAccountsTable } from 'components/modules/datatables/user';
 
 export const RequestedAccounts: NextPage = (props) => {
   const {
