569411c3c104dacdb3e6040351272ce5047fbeb7
Agu Chux <agu.chux@yahoo.com>
2023-07-13 06:36:16 +0100

    BMS: Gone 4

--- apps/bmsapp/components/modules/mdas/MdaCard.tsx
+++ apps/bmsapp/components/modules/mdas/MdaCard.tsx
@@ -14,6 +14,9 @@ import Restore from '@mui/icons-material/Restore';
 import AddCircle from '@mui/icons-material/AddCircle';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Form from 'react-bootstrap/Form';
+import { toFiat } from 'libs/monify';
+import { Row, Col } from 'react-bootstrap';
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
@@ -89,7 +92,70 @@ export const MdaCard: React.FC<CardProps> = (props: CardProps) => {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
+          <Row>
+            <Col>
+              <Form.Group controlId="fullYearActual_2021" className="tw-mt-2">
+                <Form.Label>Full Year Actuals (2021)</Form.Label>
+                <Typography className="tw-text-xl">
+                  {toFiat(Number(mda.fullYearActual_2021))}
+                </Typography>
+              </Form.Group>
+              <Form.Group controlId="approvedBudget_2022" className="tw-mt-2">
+                <Form.Label>Approved Budget (2022)</Form.Label>
+                <Typography className="tw-text-xl">
+                  {toFiat(Number(mda.approvedBudget_2022))}
+                </Typography>
+              </Form.Group>
+              <Form.Group
+                controlId="performanceBudget_2022"
+                className="tw-mt-2"
+              >
+                <Form.Label>Performance (2022) Jan-Sep</Form.Label>
+                <Typography className="tw-text-xl">
+                  {toFiat(Number(mda.performanceBudget_2022))}
+                </Typography>
+              </Form.Group>
+              <Form.Group controlId="approvedBudget_2023" className="tw-mt-2">
+                <Form.Label>Approved Budget (2023)</Form.Label>
+                <Typography className="tw-text-xl">
+                  {toFiat(Number(mda.approvedBudget_2023))}
+                </Typography>
+              </Form.Group>
+            </Col>
+
+            <Col>
+              <Form.Group controlId="personalTotal" className="tw-mt-2">
+                <Form.Label>Personal Expenditure</Form.Label>
+                <Typography className="tw-text-xl">
+                  {toFiat(Number(mda.personalTotal))}
+                </Typography>
+              </Form.Group>
+              <Form.Group controlId="overheadTotal" className="tw-mt-2">
+                <Form.Label>Overhead Expenditure</Form.Label>
+                <Typography className="tw-text-xl">
+                  {toFiat(Number(mda.overheadTotal))}
+                </Typography>
+              </Form.Group>
+              <Form.Group controlId="recurrentTotal" className="tw-mt-2">
+                <Form.Label>Recurrent Expenditure</Form.Label>
+                <Typography className="tw-text-xl">
+                  {toFiat(Number(mda.recurrentTotal))}
+                </Typography>
+              </Form.Group>
+              <Form.Group controlId="capitalTotal" className="tw-mt-2">
+                <Form.Label>Capital Expenditure</Form.Label>
+                <Typography className="tw-text-xl">
+                  {toFiat(Number(mda.capitalTotal))}
+                </Typography>
+              </Form.Group>
+              <Form.Group controlId="expenditureTotal" className="tw-mt-2">
+                <Form.Label>Total Expenditure</Form.Label>
+                <Typography className="tw-text-xl tw-text-red-500">
+                  {toFiat(Number(mda.expenditureTotal))}
+                </Typography>
+              </Form.Group>
+            </Col>
+          </Row>
         </CardContent>
       </Collapse>
     </Card>
