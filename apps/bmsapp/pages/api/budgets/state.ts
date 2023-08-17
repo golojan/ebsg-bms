a8b01ba36ac23084dbf5f7f2d206bbd3f29e20a0
Agu Chux <agu.chux@yahoo.com>
2023-08-17 22:38:28 +0100

    All up 6

--- apps/bmsapp/pages/api/budgets/state.ts
+++ apps/bmsapp/pages/api/budgets/state.ts
@@ -14,32 +14,6 @@ export default async function handler(
       where: {
         registered: true,
       },
-      _sum: {
-        overheadTotal: true,
-        capitalTotal: true,
-        recurrentTotal: true,
-        expenditureTotal: true,
-        fullYearActual_2020: true,
-        fullYearActual_2021: true,
-        fullYearActual_2022: true,
-        fullYearActual_2023: true,
-        fullYearActual_2024: true,
-        fullYearActual_2025: true,
-
-        approvedBudget_2020: true,
-        approvedBudget_2021: true,
-        approvedBudget_2022: true,
-        approvedBudget_2023: true,
-        approvedBudget_2024: true,
-        approvedBudget_2025: true,
-
-        performanceBudget_2020: true,
-        performanceBudget_2021: true,
-        performanceBudget_2022: true,
-        performanceBudget_2023: true,
-        performanceBudget_2024: true,
-        performanceBudget_2025: true,
-      },
     })
     .then((mdas) => {
       return res.status(200).json({
